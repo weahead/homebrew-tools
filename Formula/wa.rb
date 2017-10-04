c007c40b17d1be29514974c4891000f91274711f
Michael Zetterberg fd. Lopez <michael@weahead.se>
2017-10-04 09:16:03 +0200

    Upgrade WA CLI to v1.0.1

--- Formula/wa.rb
+++ Formula/wa.rb
@@ -1,7 +1,7 @@
 class Wa < Formula
   desc "We aheads CLI"
   homepage "https://github.com/weahead/cli"
-  url "https://github.com/weahead/cli.git", :tag => "v1.0.0", :revision => "748b7cdadc8f2505595139804843b00e837e3a89"
+  url "https://github.com/weahead/cli.git", :tag => "v1.0.1", :revision => "b61bc52b69c58db4e5b07cda583d64b766b7d034"
   head "https://github.com/weahead/cli.git", :branch => :master
 
   def install
